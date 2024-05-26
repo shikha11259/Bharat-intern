ae46b7941b220d5d3d53f915bfbf1983025bf2df
Shikha Rai <shikharai221106@gmail.com>
2024-05-26 19:57:21 +0530

    portfolio website

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new typed(".text", {
+     string:["Frontend Developer","Youtube", "Web Developer"],
+     typespeed:100,
+     backspeed : 100,
+     backdelay: 1000,
+     loop: true
+})
