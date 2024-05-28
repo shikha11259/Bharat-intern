d875a7fdbb6d3b0ede5369836827eb82595f7b8f
Shikha Rai <shikharai221106@gmail.com>
2024-05-28 19:48:52 +0530

    Bharatintern

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed=new typed(".text", {
-     string:["Frontend Developer","Youtube", "Web Developer"],
-     typespeed:100,
-     backspeed : 100,
-     backdelay: 1000,
-     loop: true
-})
